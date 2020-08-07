680fd616b571c2223df0838437e63ab3045fcbd9
Rafael de Lazaro <rafaeldelazaro@gmail.com>
2020-08-07 11:29:46 +0200

    login feature working with cookies

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 require('dotenv').config();
 const path = require('path');
 const express = require('express');
-const http = require('http');
-
-//A library that helps us log the requests in the console
 const logger = require('morgan');
 
 // Used to setthe favicon for our app
@@ -13,7 +10,7 @@ const cookieParser = require('cookie-parser');
 const hbs = require('hbs');
 const mongoose = require('mongoose');
 
-// Set up the database
+// Database connection
 require('./configs/db.config');
 
 const app = express();
@@ -26,36 +23,34 @@ app.use(express.static(path.join(__dirname, 'public')));
 // setting up the middleware to let it know where to find the favicon icon
 // app.use(favicon(path.join(__dirname, 'public')));
 
-// Sets up morgan in our middleware so that we can see the requests getting logged
+// Logging requests
 app.use(logger('dev'));
 
-// a body parser to allow us to parse form submissions
+// Parsing of forms
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// helps us use the cookies from each request
+// Cookies and sessions
 app.use(cookieParser());
 
-// const session = require('express-session');
-// const MongoStore = require('connect-mongo')(session);
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
  
-// app.use(session({
-//   secret: 'my-pet-cat',
-//   cookie: {
-//     maxAge: 60*60*24*1000 // 1day // in milliseconds 
-//   },
-//   store: new MongoStore({
-//     mongooseConnection: mongoose.connection,
-//     ttl: 60*60*24 // 1 day // value in seconds
-//   })
-// }));
+app.use(session({
+  secret: 'so-hungry',
+  cookie: {
+    maxAge: 60*60*24*1000*7 // 1day
+  },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 60*60*24*7 // 1 week
+  })
+}));
 
 
-// Routes middleware
+// Routers
 const indexRouter = require('./routes/index.routes');
 app.use('/', indexRouter);
 
-// const authRouter = require('./routes/auth.routes');
-// app.use('/', authRouter);
 
 module.exports = app;
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -4,24 +4,6 @@ const bcryptjs = require('bcryptjs');
 
 const {CustomerModel, BusinessModel} = require('../models/user.model');
 
-// function validateForm(name, password, usertype, email){
-//   if(!name || !email || !password){
-//     res.status(500).render('signup.hbs', {errorMessage: 'Please enter all details'})
-//     return;
-// }
-
-// const emailReg = new RegExp(/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/)
-// if (!emailReg.test(email)){
-//   res.status(500).render('signup.hbs', {errorMessage: 'Please enter valid email'})
-//   return;
-// }
-
-// const passReg = new RegExp(/^(?=.*[0-9]+.*)(?=.*[a-zA-Z]+.*)[0-9a-zA-Z]{6,}$/)
-// if (!passReg.test(password)){
-//   res.status(500).render('signup.hbs', {errorMessage: 'Password must be 6 characters and must have a nu ber and a string'})
-//   return;
-// }
-// }
 
 router.get('/', (req, res) => {
     res.render('login.hbs')
@@ -87,17 +69,11 @@ router.post('/signup', (req, res) =>{
 
 
 router.post('/signin', (req, res) => {
-  const { name, password, usertype} = req.body
+  const { userName, password, usertype} = req.body
 
-  if( !name || !password){
+  if( !userName || !password){
       res.status(500).render('login.hbs', {errorMessage: 'Please enter all details'})
       return;
-  }
-
-  const emailReg = new RegExp(/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/)
-  if (!emailReg.test(email)){
-    res.status(500).render('login.hbs', {errorMessage: 'Please enter valid email'})
-    return;
   }
 
   const passReg = new RegExp(/^(?=.*[0-9]+.*)(?=.*[a-zA-Z]+.*)[0-9a-zA-Z]{6,}$/)
@@ -105,6 +81,19 @@ router.post('/signin', (req, res) => {
     res.status(500).render('login.hbs', {errorMessage: 'Password must be 6 characters and must have a nu ber and a string'})
     return;
   }
+
+  CustomerModel.findOne({userName: userName})
+    .then((user)=>{
+      const match = bcryptjs.compare(password, user.passwordHash)
+      if (match){
+        req.session.loggedInUser = user;
+        res.redirect('/profile')
+      }
+      else{
+        res.status(500).render('auth/signin.hbs', {errorMessage: 'Password does not match.'});
+      }
+      })
+    .catch((err)=>console.log('Error is: ', err))
 
     res.redirect('/');
 });
