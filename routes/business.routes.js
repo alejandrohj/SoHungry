82cdb605f2c8f6f9ef77af1484ada7c7f3c02859
Alejandro Hernández <alejandro_hj.11@outlook.com>
2020-08-08 16:33:02 +0100

    Edit menu done

--- routes/business.routes.js
+++ routes/business.routes.js
@@ -23,8 +23,6 @@ router.get('/',(req,res)=>{
 });
 
 router.get('/menu',(req,res)=>{
-    let dishes;
-    console.log(req.session.loggedInUser._id)
     BusinessModel.findById({_id:req.session.loggedInUser._id})
         .populate('menu')
         .then((restaurantInfo)=>{
@@ -40,7 +38,6 @@ router.get('/logout',(req,res)=>{
 router.post('/', (req, res)=>{
     let {userName, cuisine, capacity, description, city, address, logo, email} = req.body
     let RestaurantID = req.session.loggedInUser._id
-    console.log (cuisine)
     BusinessModel.findByIdAndUpdate(RestaurantID, {$set: {userName, cuisine, capacity, description, location, logo, email}})
         .then(()=>redirect('/business'))
         .catch((err)=> console.log ('Could not upload the profile. Error is: ', err))
@@ -49,7 +46,6 @@ router.post('/', (req, res)=>{
 router.post('/addDish',(req,res)=>{
     const {name, price} = req.body;
     const reg = new RegExp('^[0-9]+(\.\[0-9]{1,2})?$');
-    console.log('price',price)
     if(!reg.test(price)) {
         res.status(500).render('business/menu.hbs', {errorMessage: 'Please enter a valid price: ..000.00'})
         return;
@@ -68,6 +64,15 @@ router.post('/addDish',(req,res)=>{
                 }); 
             });
     }
+});
+
+router.post('/editDish/:id',(req,res)=>{
+    const {name, price} = req.body;
+    const dishId = req.params.id;
+    DishModel.findByIdAndUpdate(dishId, {name: name, price: price})
+        .then(()=>{
+            res.redirect('/business/menu')
+        });
 });
 
 module.exports = router;
